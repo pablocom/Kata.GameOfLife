16c58e55a2da1ecfbadc7bff50e82487b79965d6
Pablo Company Ramírez <p.c.ramirez5150@gmail.com>
2021-01-02 21:14:53 +0100

    Adds alive cell neighbours recognition

--- Kata.GameOfLife.Tests/GameOfLifeShould.cs
+++ Kata.GameOfLife.Tests/GameOfLifeShould.cs
@@ -4,16 +4,74 @@ namespace Kata.GameOfLife.Tests
 {
     public class GameOfLifeShould
     {
-        [SetUp]
-        public void Setup()
+        private GameOfLife gameOfLife;
+
+        [Test]
+        public void RecognizeNoNeighborsAlive()
         {
-            
+            GivenGameOfLifeWithInitialState(new[]
+            {
+                new [] { false, false, false },
+                new [] { false, true, false },
+                new [] { false, false, false },
+            });
+
+            var neighborsAlive = gameOfLife.CountNeighborsAliveOfCell(1, 1);
+
+            Assert.That(neighborsAlive, Is.EqualTo(0));
         }
 
         [Test]
-        public void Test1()
+        public void RecognizeOneNeighborsAlive()
         {
-            Assert.Pass();
+            GivenGameOfLifeWithInitialState(new[]
+            {
+                new [] { false, false, false },
+                new [] { false, true, true },
+                new [] { false, false, false }
+            });
+
+            var neighborsAlive = gameOfLife.CountNeighborsAliveOfCell(1, 1);
+
+            Assert.That(neighborsAlive, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void RecognizeEightNeighborsAlive()
+        {
+            GivenGameOfLifeWithInitialState(new[]
+            {
+                new [] { true, true, true },
+                new [] { true, true, true },
+                new [] { true, true, true }
+            });
+
+            var neighborsAlive = gameOfLife.CountNeighborsAliveOfCell(1, 1);
+
+            Assert.That(neighborsAlive, Is.EqualTo(8));
+        }
+
+        [TestCase(0, 0)]
+        [TestCase(0, 2)]
+        [TestCase(2, 0)]
+        [TestCase(2, 2)]
+        public void RecognizeThreeNeighborsAliveAtCorners(int x, int y)
+        {
+            GivenGameOfLifeWithInitialState(new[]
+            {
+                new [] { true, true, true },
+                new [] { true, true, true },
+                new [] { true, true, true }
+            });
+
+            var neighborsAlive = gameOfLife.CountNeighborsAliveOfCell(x, y);
+
+            Assert.That(neighborsAlive, Is.EqualTo(3));
+        }
+        
+        private void GivenGameOfLifeWithInitialState(bool[][] board)
+        {
+            gameOfLife = new GameOfLife(board);
         }
     }
 }
--- Kata.GameOfLife/GameOfLife.cs
+++ Kata.GameOfLife/GameOfLife.cs
@@ -1,15 +1,43 @@
-﻿namespace Kata.GameOfLife
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kata.GameOfLife
 {
     public class GameOfLife 
     {
+        public bool[][] Board { get; private set; }
+        
         public GameOfLife(bool[][] board)
         {
-            
+            Board = board;
         }
 
         public void NextGeneration()
         {
             
+        }
+
+        public int CountNeighborsAliveOfCell(int x, int y)
+        {
+            var positionsToCheck = new List<Tuple<int, int>>();
+
+            if (x > 0) positionsToCheck.Add(new Tuple<int, int>(x - 1, y));
+            if (y > 0) positionsToCheck.Add(new Tuple<int, int>(x, y - 1));
+            if (x < Board.Length - 1) positionsToCheck.Add(new Tuple<int, int>(x + 1, y));
+            if (y < Board[0].Length - 1) positionsToCheck.Add(new Tuple<int, int>(x, y + 1));
+
+            if (x > 0 && y > 0)
+                positionsToCheck.Add(new Tuple<int, int>(x - 1, y - 1));
+            if (x < Board.Length - 1 && y < Board[0].Length - 1)
+                positionsToCheck.Add(new Tuple<int, int>(x + 1, y + 1));
+
+            if (x > 0 && y < Board[0].Length - 1)
+                positionsToCheck.Add(new Tuple<int, int>(x - 1, y + 1));
+            if (x < Board.Length - 1 && y > 0)
+                positionsToCheck.Add(new Tuple<int, int>(x + 1, y - 1));
+
+            return positionsToCheck.Count(positionToCheck => Board[positionToCheck.Item1][positionToCheck.Item2]);
         }
     }
 }
